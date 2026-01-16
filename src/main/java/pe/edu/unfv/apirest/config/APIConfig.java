48ed9cdd056a34cf015195faa2dea0699df1c823
unknown <Eleazar_Alfredo_Alfriadez_Yriarte>
2026-01-16 12:42:27 -0500

    Carga de imagen y selector de imagen ok

--- src/main/java/pe/edu/unfv/apirest/config/APIConfig.java
+++ src/main/java/pe/edu/unfv/apirest/config/APIConfig.java
@@ -2,5 +2,5 @@ package pe.edu.unfv.apirest.config;
 
 public class APIConfig {
 
-    public static final String BASE_URL = "http://192.168.1.5:3000";
+    public static final String BASE_URL = "http://192.168.68.59:3000";
 }
