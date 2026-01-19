3082e20246c8b298dc3671e1fe5a25a8c615a000
unknown <Eleazar_Alfredo_Alfriadez_Yriarte>
2026-01-19 14:19:45 -0500

    Seguimos revizando la actulizacion del 'PROFILE'

--- src/main/java/pe/edu/unfv/apirest/config/APIConfig.java
+++ src/main/java/pe/edu/unfv/apirest/config/APIConfig.java
@@ -2,5 +2,5 @@ package pe.edu.unfv.apirest.config;
 
 public class APIConfig {
 
-    public static final String BASE_URL = "http://192.168.1.2:3000";
+    public static final String BASE_URL = "http://192.168.68.54:3000";
 }
